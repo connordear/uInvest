0694951997cfac2db0b2fc0033e0b81361e051df
Connor Dear <connordear@gmail.com>
2018-05-05 16:19:25 -0400

    fixed app port address

--- server/server.js
+++ server/server.js
@@ -21,6 +21,7 @@ app.use(bodyParser.json());                                     // parse applica
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
 app.use(cors());
+app.use(express.static("www"));
 
 app.use(function(req, res, next) {
    res.header("Access-Control-Allow-Origin", "*");
